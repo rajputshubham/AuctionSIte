804cf29b0eb3ec0990b88c2631c9d8ff588b2ac9
gursimar1997 <gursimar.chopra@mountblue.io>
2018-10-13 12:34:28 +0530

    changed database to rds

--- AuctionSite/settings.py
+++ AuctionSite/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'AuctionSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'ddp',
+       'USER': 'ashish',
+       'PASSWORD': 'test1234',
+       'HOST': 'mydb.cviaagoh6lmy.us-east-2.rds.amazonaws.com',
+       'PORT': '3306',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
